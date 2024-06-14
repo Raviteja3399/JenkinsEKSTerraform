ef9278415c4b456a477ba0f055ef3fc933c72d0a
NIDADAVOLU RAVITEJA <52151482+Raviteja3399@users.noreply.github.com>
2024-06-14 20:10:33 +0530

    Delete Jenkinsserver directory

--- Jenkinsserver/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "jenkinsartifacts"
-    key    = "jenkins/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
--- Jenkinsserver/terraform.tfvars
+++
@@ -1,5 +0,0 @@
-Vpc_CIDR = "10.0.0.0/24"
-
-public_subnets = ["10.0.0.0/28"]
-
-instance_type = "t2.micro"
